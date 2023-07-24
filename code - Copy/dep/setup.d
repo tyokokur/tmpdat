bbe87f5b161b95ab39aa898080469ceacfa7a5c0
tyokokur <takashi.yokokura@berkeley.edu>
2023-07-24 12:48:58 -0700

    autocommit

--- code - Copy/dep/setup.d
+++
@@ -1,7 +0,0 @@
-setup.o: bin/src/setup.c bin/src/setup.h bin/src/common.h bin/src/gets.h \
- bin/src/report.h
-bin/src/setup.c:
-bin/src/setup.h:
-bin/src/common.h:
-bin/src/gets.h:
-bin/src/report.h:
