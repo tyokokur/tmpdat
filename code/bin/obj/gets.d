56f32eb0b0da4e8b5f6d3ea2d3834068145fd2a7
tyokokur <110191996+tyokokur@users.noreply.github.com>
2023-07-26 10:23:38 -0700

    autocommit

--- code/bin/obj/gets.d
+++
@@ -1,5 +0,0 @@
-bin/obj/gets.o: bin/src/gets.c bin/src/gets.h bin/src/common.h
-
-bin/src/gets.h:
-
-bin/src/common.h:
--- code/bin/obj/report.d
+++
@@ -1,5 +0,0 @@
-bin/obj/report.o: bin/src/report.c bin/src/report.h bin/src/common.h
-
-bin/src/report.h:
-
-bin/src/common.h:
